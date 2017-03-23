2a531fd340397d066a614c61dbc9b6889e025454
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2017-03-23 14:38:08 -0400

    Create untrack.php

---
+++ untrack.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Get Untracked</title>
+</head>
+<body>
+Tracking has been disabled for this server<Br/>
+We reccomend keeping tracking on in order to help repo owners figure out your interests, and change their content to help you.<br/>
+<br/>
+<br/>
+
+Other servers may still have tracking on. <br/>
+<Br/>
+<br/>
+All READMEs that have a tracking set up linked to here will now show a green-on-black image that says tracking disabled.
+
+</body>
+</html>

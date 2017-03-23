125803092bd7a86bcebe7fcd1b49bd7951acb380
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2017-03-23 15:14:03 -0400

    Update init.php

--- init.php
+++ init.php
@@ -2,18 +2,28 @@
 
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-</head>
-<body>
-<form action="init.php" method="GET">
-<label for="hostname">Hostname: </lable><input name="hostname" type="text"/><br/>
-<label for="port">Port: </lable><input name="port" type="text"/><br/>
-<label for="password">Password: </lable><input type="password"/><br/>
-<b> In cPanel, make sure that you allow your own IP to access your own database!!! </b>
-</form>
-</body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <form action="init.php" method="GET">
+      <label for="hostname">Hostname: </label><input name="hostname" type="text"/><br/>
+      <label for="port">Port: </label><input name="port" type="text"/><br/>
+      <label for="password">Password: </label><input type="password"/><br/>
+      <b> In cPanel, make sure that you allow your own IP to access your own database!!! </b>
+    </form>
+    
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>

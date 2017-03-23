bf3df3e5cc26234f02e90e363dc78fc076b3bad9
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2017-03-23 15:10:31 -0400

    Create init.php

---
+++ init.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+<form action="init.php" method="GET">
+<label for="hostname">Hostname: </lable><input name="hostname" type="text"/><br/>
+<label for="port">Port: </lable><input name="port" type="text"/><br/>
+<label for="password">Password: </lable><input type="password"/><br/>
+<b> In cPanel, make sure that you allow your own IP to access your own database!!! </b>
+</form>
+</body>
+</html>
